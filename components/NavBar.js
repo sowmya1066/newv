f843427de16cdac243b505245ab7493a772e8415
Sowmya G S <sowmyags1066@gmail.com>
2024-08-05 19:46:26 +0530

    hero app router

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,9 +8,9 @@ const NavBar = () => {
         <img src="/image.png" alt="Logo" />
       </div>
       <div className={styles.navLinks}>
-        <Link href="/nav1">Nav1</Link>
-        <Link href="/nav2">Nav2</Link>
-        <Link href="/nav3">Nav3</Link>
+        <Link href="/nav1">Skill</Link>
+        <Link href="/nav2">Work</Link>
+        <Link href="/nav3">Contact</Link>
       </div>
       <div className={styles.resume}>
         <Link href="/resume">Resume</Link>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 const HomePage = () => {
   return (
-    <div>
+    <div className="hero">
+      <img src="/hero-img.png" alt="guy saying hi" className="heroImg" />
       <h1>Welcome to my Next.js app!</h1>
     </div>
   );
